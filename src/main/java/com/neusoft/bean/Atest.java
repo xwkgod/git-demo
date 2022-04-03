4a7274c1d92f8f02e0f07c354d117d0af82ff6da
xunwenkuo <1393284796@qq.com>
2022-04-03 17:11:33 +0800

    first demo

---
+++ src/main/java/com/neusoft/bean/Atest.java
@@ -0,0 +1,12 @@
+package com.neusoft.bean;
+
+/**
+ * @Author xwk_start
+ * @Date 2022/4/3 16:47
+ * @Description
+ */
+public class Atest {
+    public static void main(String[] args) {
+        System.out.println("嘿嘿");
+    }
+}
---
+++ src/main/java/com/neusoft/bean/Teste.java
@@ -0,0 +1,12 @@
+package com.neusoft.bean;
+
+/**
+ * @Author xwk_start
+ * @Date 2022/4/3 16:45
+ * @Description
+ */
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("来了");
+    }
+}
